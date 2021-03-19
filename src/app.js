b18cd11e5cdbe3f71624b57b00d86812c53ce0f8
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-19 12:45:18 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Set.
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
